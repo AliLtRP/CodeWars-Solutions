a3a74e07835f824eb62905e12dacc519f8ceeb53
AliLtRP <albaghdadyali1@gmail.com>
2022-11-25 09:35:29 +0300

    Update

---
+++ 8kyu-java/SumArray.java
@@ -0,0 +1,15 @@
+
+public class SumArray {
+
+	public static double sum(double[] numbers) {	    
+		
+		double result = 0;
+		
+		for (double i: numbers) {
+			result+=i;
+		}
+		return result;
+	  }
+}
+
+
---
+++ 8kyu/SumArrays.py
@@ -0,0 +1,7 @@
+def sum_array(a):
+    result = 0
+    
+    for i in a:
+        result +=i
+    
+    return 0 if len(a) == 0 else result
